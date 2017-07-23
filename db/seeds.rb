dc7a89cbf24e3b912cd93f8ef4074a4945018e43
Dan Collis-Puro <dan@collispuro.net>
2017-07-23 14:27:35 -0400

    215 tickets as a default

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Raffle.create!(name: 'Big Money', number_of_tickets: 250)
+Raffle.create!(name: 'Big Money', number_of_tickets: 215)
